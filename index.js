bb2cd605ac7f99ddfc947d6c9e36fa78b2c69ceb
Scott Quach <scottqglobal@gmail.com>
2026-03-22 14:54:58 -0700

    feat: wire scheduleJobs into startup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const { join } = require('node:path');
 const { createBotFromDirectory } = require('./src/bot-factory');
+const { scheduleJobs } = require('./src/job-scheduler');
 
 const token = process.env.BOT_TOKEN;
 const bot = new Telegraf(token);
@@ -10,6 +11,7 @@ createBotFromDirectory(bot, join(__dirname, 'bots'));
 
 console.log('Bot is running...');
 bot.launch();
+scheduleJobs(bot, join(__dirname, 'jobs'));
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
